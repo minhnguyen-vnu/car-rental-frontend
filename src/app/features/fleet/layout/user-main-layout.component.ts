8d831efca8dd2f6207d3e69fb8ce72dc48269d0f
th1enq <23020096@vnu.edu.vn>
2025-12-07 17:15:46 +0700

    save

--- src/app/features/fleet/layout/user-main-layout.component.ts
+++ src/app/features/fleet/layout/user-main-layout.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from '../search/search.component';
 import { RentalSearchComponent } from '../../rental/search/search.component'; 
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-user-main-layout',
@@ -15,9 +16,14 @@ import { RentalSearchComponent } from '../../rental/search/search.component';
   styleUrl: './user-main-layout.component.css'
 })
 export class UserMainLayoutComponent {
+  
+  constructor(private authService: AuthService) {}
   activeTab: string = 'vehicle-search'; // tab mặc định khi mở
 
   setTab(tab: string): void {
     this.activeTab = tab;
   }
+  logout(): void {
+    this.authService.logout();
+   }
 }
